3d97e07f4487ec0095108e46dd9f7a2664f73378
Evan Hildreth <me@eph.me>
2016-03-26 15:46:16 -0400

    Compare page WIP

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,11 @@
 class DashboardController < ApplicationController
-    skip_before_action :admin_only, only: [:home]
+    skip_before_action :admin_only, only: [:home, :compare]
 
     def home
+    end
+    
+    def compare
+        @left_service = Service.find(params[:left])
+        @right_service = Service.find(params[:right])
     end
 end
