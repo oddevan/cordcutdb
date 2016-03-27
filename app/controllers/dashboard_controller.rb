df258ff3c79d0fe842494c1576304a7623ea6d48
Evan Hildreth <me@eph.me>
2016-03-27 13:40:07 -0400

    More beginnings of conparison

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,6 +6,15 @@ class DashboardController < ApplicationController
     
     def compare
         @left_service = Service.find(params[:left])
+        @left_channels = []
+        @left_service.tiers.each do |tier|
+            @left_channels = @left_channels | tier.channels
+        end
+        
         @right_service = Service.find(params[:right])
+        @right_channels = []
+        @right_service.tiers.each do |tier|
+            @right_channels = @right_channels | tier.channels
+        end
     end
 end
