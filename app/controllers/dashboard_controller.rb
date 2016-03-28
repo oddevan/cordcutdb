83ce99dcb54184bdb128f6a5ffe6878024fd0a4b
Evan Hildreth <me@eph.me>
2016-03-27 20:20:34 -0400

    Almost there on comparison!

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,16 +5,41 @@ class DashboardController < ApplicationController
     end
     
     def compare
+        @all_channels = [];
+    
         @left_service = Service.find(params[:left])
         @left_channels = []
         @left_service.tiers.each do |tier|
             @left_channels = @left_channels | tier.channels
+            @all_channels = @all_channels | tier.channels
+            
+            tier.channels.each do |channel|
+                curchan = @all_channels[@all_channels.index(channel)]
+                if curchan.css_class
+                    curchan.css_class += " tier#{tier.id}"
+                else
+                    curchan.css_class = "tier#{tier.id}"
+                end
+            end
         end
         
         @right_service = Service.find(params[:right])
         @right_channels = []
         @right_service.tiers.each do |tier|
             @right_channels = @right_channels | tier.channels
+            @all_channels = @all_channels | tier.channels
+            
+            tier.channels.each do |channel|
+                curchan = @all_channels[@all_channels.index(channel)]
+                if curchan.css_class
+                    curchan.css_class += " tier#{tier.id}"
+                else
+                    curchan.css_class = "tier#{tier.id}"
+                end
+            end
         end
+        
+        #@all_channels = @left_channels | @right_channels
+        @all_channels.sort! { |a,b| a.name <=> b.name }
     end
 end
