44cb53c221954425b4e9e3f8c8c37d01ca9b2ad3
Evan Hildreth <me@eph.me>
2016-03-27 20:20:34 -0400

    Almost there on comparison!

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -2,4 +2,6 @@ class Channel < ActiveRecord::Base
     has_and_belongs_to_many :tiers
     has_and_belongs_to_many :locks
     default_scope { order(name: :asc) }
+    
+    attr_accessor :css_class
 end
