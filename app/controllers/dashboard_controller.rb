f1f2dcb153835d563cf0fb95e12a94186d6e2a03
Evan Hildreth <me@eph.me>
2016-04-10 12:32:24 -0400

    Fixing bug and making sure home page has a description

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
     skip_before_action :admin_only #, only: [:home, :compare]
 
     def home
-        @service = Service.order("RANDOM()").first
+        @service = Service.where.not(description: '').order("RANDOM()").first
     end
     
     def faq
