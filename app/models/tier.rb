c962342d2aa2b4b6dcd1429a592dd4d3112facb0
Evan Hildreth <me@eph.me>
2016-04-24 16:02:27 -0400

    Better tier display

--- app/models/tier.rb
+++ app/models/tier.rb
@@ -1,8 +1,11 @@
 class Tier < ActiveRecord::Base
   belongs_to :service
-  belongs_to :prereq, class_name: "Tier"
   has_and_belongs_to_many :channels
   has_many :locks
   has_and_belongs_to_many :regions
+  
+  has_many :addons, class_name: "Tier", foreign_key: 'prereq_id'
+  belongs_to :prereq, class_name: "Tier"
+  
   default_scope { order(name: :asc) }
 end
